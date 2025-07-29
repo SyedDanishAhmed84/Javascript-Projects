9e76f06005e85bde65d2a3d66afa805e6886b0f2
Syed Danish Ahmed <daniahmed758@gmail.com>
2025-07-29 18:56:12 +0500

    script.js

--- Basic Counter/script.js
+++ Basic Counter/script.js
@@ -5,12 +5,12 @@ document.getElementById("click-btn").addEventListener("click", function() {
   document.getElementById("counter").innerText = count;
 });
 
-document.getElementById("decrement-btn").addEventListener("click",function(){
-  count=count-1;
+document.getElementById("reset-btn").addEventListener("click",function(){
+  count=0
   document.getElementById("counter").innerText=count
 });
 
-document.getElementById("reset-btn").addEventListener("click",function(){
-  count=0
+document.getElementById("decrement-btn").addEventListener("click",function(){
+  count=count-1;
   document.getElementById("counter").innerText=count
 });
